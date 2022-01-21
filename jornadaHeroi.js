0f526f7241c1301a145f9e2312393036e73139e2
Rhuan Lemes <45194959+RhuanVL@users.noreply.github.com>
2022-01-20 20:14:51 -0400

    Update jornadaHeroi.js

--- jornadaHeroi.js
+++ jornadaHeroi.js
@@ -1,6 +1,10 @@
 const prompt = require("prompt-sync")();
 
+function pause (time) {
+  return new Promise((rep) => setTimeout(rep, time));
+}
 
+pause(300).then(() => {               
 console.log(`\nA história do nosso herói começa a muito tempo atrás em uma terra muito distante, onde combates mortais eram muito comuns,
 dragões, fanstasmas e demônios coexistiam nesse mundo com os humanos. Num certo dia nosso herói chamado Vlad, teve a missão matar o dragão 
 Bermogan e também de encontrar o filho do imperador que desapareceu tentando derrotar o temido dragão, missão dada pelo imperador Castellia, 
@@ -8,13 +12,14 @@ imperador da nação Lucciz, sendo assim, nosso herói precisa viajar até os bo
 consistia em 3 dias e 3 noites até o local e durante essa jornada, nosso herói teve muitas surpresas e desafios para solucionar, tais como
 lidar com quadrilha de ladrões, demônios que o perseguia constantemente, escassez de alimentos de água, um urso gigante que o tentou atacar
 enquanto ele dormia...\n`);
-
+}
+    
 let sim = 0;
 
 //1º pergunta
 const seuCavaloSobreviveu = prompt("O cavalo de Vlad conseguiu sobreviver até o combate final? sim ou não? ");
 
-if(seuCavaloSobreviveu === "Sim" || seuCavaloSobreviveu === "sim" || seuCavaloSobreviveu === "s")
+if(seuCavaloSobreviveu === "Sim" || seuCavaloSobreviveu === "SIM" || seuCavaloSobreviveu === "sim" || seuCavaloSobreviveu === "s")
 {
     sim += 1;
 }
@@ -25,7 +30,7 @@ else{
 //2º pergunta
 const pegouPocoesMagicas = prompt("Vlad obteve todas as poções mágicas durante sua jornada e utilizou contra bergamon? sim ou não? ");
 
-if(pegouPocoesMagicas === "Sim" || pegouPocoesMagicas === "sim" || pegouPocoesMagicas === "s")
+if(pegouPocoesMagicas === "Sim" || pegouPocoesMagicas === "SIM" || pegouPocoesMagicas === "sim" || pegouPocoesMagicas === "s")
 {
     sim += 1;
 }
@@ -36,7 +41,7 @@ else{
 //3º pergunta
 const arrancouCabeca = prompt("Vlad consegiu derrotar bergamon, ele arrancou sua cabeça com premio para levar para o imperador? sim ou não ");
 
-if(arrancouCabeca === "Sim" || arrancouCabeca === "sim" || arrancouCabeca === "s")
+if(arrancouCabeca === "Sim" || arrancouCabeca === "SIM" || arrancouCabeca === "sim" || arrancouCabeca === "s")
 {
     sim += 1;
 }
@@ -47,7 +52,7 @@ else{
 //4º pergunta
 const descobriuBauSecreto = prompt("Vlad encontrou o Bau secreto que continha uma arma formidável dentro da caverna de Bergamon? sim ou não? ");
 
-if(descobriuBauSecreto === "Sim" || descobriuBauSecreto === "sim" || descobriuBauSecreto === "s")
+if(descobriuBauSecreto === "Sim" || descobriuBauSecreto === "SIM" || descobriuBauSecreto === "sim" || descobriuBauSecreto === "s")
 {
     sim += 1;
 }
@@ -58,7 +63,7 @@ else{
 //5º pergunta
 const encontrouJovem = prompt("Vlad encontrou o jovem filho do imperador no final da sua Jornada? sim ou não? ");
 
-if(encontrouJovem === "Sim" || encontrouJovem === "sim" || encontrouJovem === "s")
+if(encontrouJovem === "Sim" || encontrouJovem === "SIM" || encontrouJovem === "sim" || encontrouJovem === "s")
 {
     sim += 1;
 }
@@ -87,3 +92,5 @@ else if(sim === 5){
     console.log("\nVocê cumpriu de maneira perfeita sua missão, e o Imperador o nomeou o novo herói da nação de Lucciz.");
     console.log("\n####GAME OVER####");
 }
+
+Console.log("\tObrigado Por Jogar!");
